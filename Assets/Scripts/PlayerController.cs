cb65fbd1c4f11aaa2aa5e697e68ced06f9b68feb
samfrack <89489598+samfrack@users.noreply.github.com>
2021-11-18 17:44:40 -0600

    Activity 2.5 Complete

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2> ();
+    }
+}
